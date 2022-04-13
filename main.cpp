f8beaf5b78d973bee5d6901cd3fef67399557684
JunyaSugita <k021g1293@g.neec.ac.jp>
2022-04-13 14:47:55 +0900

    ウインドウプロシージャ完成

--- main.cpp
+++ main.cpp
@@ -7,3 +7,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	return 0;
 }
+
+//ウインドウプロシージャ
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+	//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの修了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
