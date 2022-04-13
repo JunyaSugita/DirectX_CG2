5b74aa763e4822fa437899d13e06495c0d694fa9
JunyaSugita <k021g1293@g.neec.ac.jp>
2022-04-13 14:25:48 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+//windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//コンソールへの文字入力
+	OutputDebugStringA("Hello,DirectX!!\n");
+
+	return 0;
+}
