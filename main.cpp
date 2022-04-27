e2c9526212c31f80122e9cad6f621f1d351bad62
JunyaSugita <k021g1293@g.neec.ac.jp>
2022-04-27 13:34:35 +0900

    ビューポート設定

--- main.cpp
+++ main.cpp
@@ -453,9 +453,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		// ビューポート設定コマンド
 		D3D12_VIEWPORT viewport{};
-		viewport.Width = window_width;
+		viewport.Width = window_width / 2;
 		viewport.Height = window_height;
-		viewport.TopLeftX = 0;
+		viewport.TopLeftX = window_width / 2;
 		viewport.TopLeftY = 0;
 		viewport.MinDepth = 0.0f;
 		viewport.MaxDepth = 1.0f;
